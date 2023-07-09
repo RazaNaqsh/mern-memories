bea81fe9721d904c8efc361da1c036c0568d9375
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-09 17:44:46 +0530

    Clear editing form

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -39,7 +39,16 @@ const Form = ({ currentId, setCurrentId }) => {
     }
   };
 
-  const clear = () => {};
+  const clear = () => {
+    setCurrentId(null);
+    setPostData({
+      creator: "",
+      title: "",
+      message: "",
+      tags: "",
+      selectedFile: "",
+    });
+  };
 
   return (
     <Paper className={classes.paper}>
@@ -49,7 +58,9 @@ const Form = ({ currentId, setCurrentId }) => {
         className={`${classes.root} ${classes.form}`}
         onSubmit={handleSubmit}
       >
-        <Typography variant="h6">Creating a Memory</Typography>
+        <Typography variant="h6">
+          {currentId ? "Editing" : "Creating"} a Memory
+        </Typography>
         <TextField
           name="creator"
           variant="outlined"
