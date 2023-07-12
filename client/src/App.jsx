2ae4d6f0d1e5162c799f6e03b9927af9df957911
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-12 21:23:02 +0530

    Export elements to home component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 
-import Form from "./components/Form/Form.jsx";
-import Posts from "./components/Posts/Posts.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import useStyles from "./styles";
-
-import { useDispatch } from "react-redux";
-import { getPosts } from "./actions/posts";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home.jsx";
 
 function App() {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
-
   return (
-    <Container maxwidth="lg">
-      <Navbar />
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            className={classes.mainContainer}
-            justifyContent="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxwidth="lg">
+        <Navbar />
+        <Home />
+      </Container>
+    </BrowserRouter>
   );
 }
 
