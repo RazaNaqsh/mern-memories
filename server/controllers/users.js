8b01dda3015a82c2c31369c7f1db8b4342e321a8
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-14 19:56:55 +0530

    Sign in controller

---
+++ server/controllers/users.js
@@ -0,0 +1,37 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import User from "../models/user.js";
+
+export const singin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(404).json({ message: "User does not exists" });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid Credentials" });
+    }
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      process.env.SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.status(500).json({ result: existingUser, token });
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong.." });
+  }
+};
+
+export const signup = async (req, res) => {};
